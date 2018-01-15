b7d22525f2d6c72186c4c27dad14a7a694f71a36
Jakub Komorowski <jakub.komorowski@10clouds.com>
2018-01-15 16:40:58 +0100

    Added track gradient

--- gaugeseekbar/src/main/java/com/tenclouds/gaugeprogress/GaugeSeekBar.kt
+++ gaugeseekbar/src/main/java/com/tenclouds/gaugeprogress/GaugeSeekBar.kt
@@ -14,7 +14,6 @@ class GaugeSeekBar : View {
         private const val START_ANGLE_DEG = 30f
         private const val DEFAULT_THUMB_RADIUS_DP = 11
         private const val DEFAULT_TRACK_WIDTH_DP = 8
-        private const val DEFAULT_TRACK_COLOR = -3090191
     }
 
     constructor(context: Context) : super(context)
@@ -28,9 +27,9 @@ class GaugeSeekBar : View {
 
     private var thumbRadius = DEFAULT_THUMB_RADIUS_DP * resources.displayMetrics.density
     private var trackWidth = DEFAULT_TRACK_WIDTH_DP * resources.displayMetrics.density
-    private var trackColor: Int = DEFAULT_TRACK_COLOR
-    private var gradientArray = context.resources.getIntArray(R.array.index_gradient)
-    private var gradientArrayPositions: FloatArray? = null
+    private var trackGradientArray: IntArray = context.resources.getIntArray(R.array.default_track_gradient)
+    private var progressGradientArray = context.resources.getIntArray(R.array.default_index_gradient)
+    private var progressGradientArrayPositions: FloatArray? = null
 
     private var trackDrawable: TrackDrawable? = null
     private var progressDrawable: ProgressDrawable? = null
@@ -60,19 +59,29 @@ class GaugeSeekBar : View {
     private fun applyAttributes(attributes: TypedArray) {
         try {
             thumbRadius = attributes.getDimension(R.styleable.GaugeSeekBar_thumbRadius, thumbRadius)
-            trackColor = attributes.getColor(R.styleable.GaugeSeekBar_trackColor, trackColor)
+            val trackGradientArrayId = attributes.getResourceId(R.styleable.GaugeSeekBar_trackGradient, 0)
+            if (trackGradientArrayId != 0) {
+                trackGradientArray = resources.getIntArray(trackGradientArrayId)
+            }
+
+            val trackGradientArrayPositionsResourceId = attributes.getResourceId(R.styleable.GaugeSeekBar_trackGradientPositions, 0)
+            if (trackGradientArrayPositionsResourceId != 0) {
+                val positionsIntArray = resources.getIntArray(trackGradientArrayPositionsResourceId)
+                progressGradientArrayPositions = FloatArray(positionsIntArray.size) { positionsIntArray[it].div(100f) }
+            }
+
             showThumb = attributes.getBoolean(R.styleable.GaugeSeekBar_showThumb, showThumb)
             trackWidth = attributes.getDimension(R.styleable.GaugeSeekBar_trackWidth, trackWidth)
             progress = attributes.getFloat(R.styleable.GaugeSeekBar_progress, 0f)
 
             val gradientArrayResourceId = attributes.getResourceId(R.styleable.GaugeSeekBar_progressGradient, 0)
             if (gradientArrayResourceId != 0) {
-                gradientArray = resources.getIntArray(gradientArrayResourceId)
+                progressGradientArray = resources.getIntArray(gradientArrayResourceId)
             }
             val gradientArrayPositionsResourceId = attributes.getResourceId(R.styleable.GaugeSeekBar_progressGradientPositions, 0)
             if (gradientArrayPositionsResourceId != 0) {
                 val positionsIntArray = resources.getIntArray(gradientArrayPositionsResourceId)
-                gradientArrayPositions = FloatArray(positionsIntArray.size) { positionsIntArray[it].div(100f) }
+                progressGradientArrayPositions = FloatArray(positionsIntArray.size) { positionsIntArray[it].div(100f) }
             }
 
         } finally {
@@ -90,9 +99,9 @@ class GaugeSeekBar : View {
         val centerPosition = PointF(centerX, centerY)
         val radiusPx = Math.min(centerX, centerY)
         val margin = Math.max(thumbRadius, trackWidth / 2f)
-        trackDrawable = TrackDrawable(centerPosition, radiusPx, margin, trackColor, START_ANGLE_DEG, trackWidth)
-        progressDrawable = ProgressDrawable(centerPosition, 0f, radiusPx, margin, gradientArray, START_ANGLE_DEG, trackWidth, gradientArrayPositions)
-        thumbDrawable = ThumbDrawable(centerPosition, 0f, gradientArray, START_ANGLE_DEG, thumbRadius)
+        trackDrawable = TrackDrawable(centerPosition, radiusPx, margin, trackGradientArray, START_ANGLE_DEG, trackWidth)
+        progressDrawable = ProgressDrawable(centerPosition, 0f, radiusPx, margin, progressGradientArray, START_ANGLE_DEG, trackWidth, progressGradientArrayPositions)
+        thumbDrawable = ThumbDrawable(centerPosition, 0f, progressGradientArray, START_ANGLE_DEG, thumbRadius)
     }
 
     override fun onDraw(canvas: Canvas?) {
--- gaugeseekbar/src/main/java/com/tenclouds/gaugeprogress/TrackDrawable.kt
+++ gaugeseekbar/src/main/java/com/tenclouds/gaugeprogress/TrackDrawable.kt
@@ -2,31 +2,63 @@ package com.tenclouds.gaugeprogress
 
 import android.graphics.*
 
-class TrackDrawable(centerPosition: PointF,
+class TrackDrawable(position: PointF,
                     private val radiusPx: Float,
                     private val margin: Float,
-                    private val color: Int,
+                    private val gradientArray: IntArray,
                     private val startAngle: Float,
-                    private val trackWidthPx: Float) : DrawableEntity(centerPosition) {
+                    private val trackWidthPx: Float,
+                    gradientPositionsArray: FloatArray? = null) : DrawableEntity(position) {
 
-    private val paint = Paint().apply {
-        color = this@TrackDrawable.color
+    private val gradientPositionsArray: FloatArray = gradientPositionsArray ?: FloatArray(gradientArray.size) { it.toFloat() / gradientArray.size }
+
+    init {
+        if (gradientArray.size != this.gradientPositionsArray.size)
+            throw IllegalStateException("gradientArray and gradientPositionsArray sizes must be equal.")
+    }
+
+    private val progressPaint = Paint().apply {
         strokeWidth = trackWidthPx
         isAntiAlias = true
         strokeCap = Paint.Cap.ROUND
         style = Paint.Style.STROKE
+
+        if (gradientArray.size > 1) {
+            val shader = createSweepGradient()
+            setShader(shader)
+        } else {
+            color = gradientArray[0]
+        }
+    }
+
+    private fun createSweepGradient(): SweepGradient {
+        val shader = SweepGradient(centerPosition.x, centerPosition.y, gradientArray, getGradientPositions())
+        val gradientRotationMatrix = Matrix()
+        gradientRotationMatrix.preRotate(90f + startAngle - 5, centerPosition.x, centerPosition.y)
+        shader.setLocalMatrix(gradientRotationMatrix)
+        return shader
+    }
+
+    private fun getGradientPositions(): FloatArray {
+        val normalizedStartAngle = startAngle / 360f
+        val normalizedAvailableSpace = 1f - 2 * normalizedStartAngle
+
+        return FloatArray(gradientArray.size) {
+            normalizedStartAngle + normalizedAvailableSpace * gradientPositionsArray[it]
+        }
     }
 
     override fun draw(canvas: Canvas) {
+        val angle = (360 - (startAngle * 2))
         val rect = RectF(centerPosition.x - radiusPx + margin,
                 centerPosition.y - radiusPx + margin,
                 centerPosition.x + radiusPx - margin,
                 centerPosition.y + radiusPx - margin)
         canvas.drawArc(rect,
                 90f + startAngle,
-                360f - 2 * startAngle,
+                angle,
                 false,
-                paint)
+                progressPaint)
     }
 
     override fun setAlpha(alpha: Int) {}
