d66fc15d3783c7db4e5e6de9645ae33bdad33dc4
Jakub Komorowski <jakub.komorowski@10clouds.com>
2018-01-17 13:39:04 +0100

    Changes in MainActivity

--- sampleapp/src/main/java/com/tenclouds/gaugeprogressbar/MainActivity.kt
+++ sampleapp/src/main/java/com/tenclouds/gaugeprogressbar/MainActivity.kt
@@ -2,11 +2,24 @@ package com.tenclouds.gaugeprogressbar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initProgress1View()
+    }
+
+    private fun initProgress1View() {
+        val initialProgress = 0.1f
+        progressText.text = String.format("%.2f", initialProgress)
+        progress1.progress = initialProgress
+
+        progress1.progressChangedCallback = {
+            progressText.text = String.format("%.2f", it)
+        }
     }
 }
