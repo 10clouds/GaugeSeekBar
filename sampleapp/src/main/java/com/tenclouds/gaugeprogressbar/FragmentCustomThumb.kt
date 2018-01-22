bd1cddd3755f1d4b7c2ef414f535b1e9f22a0eee
Jakub Komorowski <jakub.komorowski@10clouds.com>
2018-01-22 12:18:30 +0100

    Added updating progress text in FragmentCustomThumb and FragmentUnstyledSeekbar.

--- sampleapp/src/main/java/com/tenclouds/gaugeprogressbar/FragmentCustomThumb.kt
+++ sampleapp/src/main/java/com/tenclouds/gaugeprogressbar/FragmentCustomThumb.kt
@@ -5,9 +5,15 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_custom_thumb.*
+import kotlinx.android.synthetic.main.fragment_custom_thumb.view.*
 
 class FragmentCustomThumb : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_custom_thumb, container, false)
+        val view = inflater.inflate(R.layout.fragment_custom_thumb, container, false)
+        view.progress.progressChangedCallback = {
+            progressText.text = String.format("%.2f", it)
+        }
+        return view
     }
 }
--- sampleapp/src/main/java/com/tenclouds/gaugeprogressbar/FragmentUnstyledSeekbar.kt
+++ sampleapp/src/main/java/com/tenclouds/gaugeprogressbar/FragmentUnstyledSeekbar.kt
@@ -5,9 +5,15 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_unstyled_seekbar.*
+import kotlinx.android.synthetic.main.fragment_unstyled_seekbar.view.*
 
 class FragmentUnstyledSeekbar : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_unstyled_seekbar, container, false)
+        val view = inflater.inflate(R.layout.fragment_unstyled_seekbar, container, false)
+        view.progress.progressChangedCallback = {
+            progressText.text = String.format("%.2f", it)
+        }
+        return view
     }
 }
